7280aee8b6ec4808fa5b312a228b5ec3f767496b
beaustar2 <144544990+beaustar2@users.noreply.github.com>
2023-11-27 17:01:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -35,5 +35,5 @@ def about():
 def contact():
     return render_template('contact.html')
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
